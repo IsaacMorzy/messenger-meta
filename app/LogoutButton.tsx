7ef604b588f24c05d1e9afab18bd628bdee67208
IsaacMorzy <musyokaisaac98@gmail.com>
2022-11-11 20:12:00 +0300

    initialise build ;header section with session

---
+++ app/LogoutButton.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+function LogoutButton() {
+  return (
+    <button
+      onClick={() => console.log("logout clicked")}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      {" "}
+      Sign Out{" "}
+    </button>
+  );
+}
+
+export default LogoutButton;
