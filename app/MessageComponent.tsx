5777ef14c5ff4ffa8f739f9e7e2b25cec269d15b
Your Name <morzyzack@gmail.com>
2022-12-22 11:09:39 +0300

    messagecomponent:pusher

--- app/MessageComponent.tsx
+++ app/MessageComponent.tsx
@@ -6,9 +6,10 @@ type Props = {
 };
 
 function MessageComponent({ message }: Props) {
+  const isUser = false;
   return (
-    <div>
-      <div>
+    <div className={`flex w-fit ${isUser && "ml-auto"}`}>
+      <div className={`flex-shrink-0 ${isUser && "order-2"}`}>
         <Image
           className="rounded-full mx-2"
           height={10}
@@ -18,13 +19,29 @@ function MessageComponent({ message }: Props) {
         />
       </div>
       <div>
-        <p className="text-[0.65rem px-[2px] pb-[2px] text-red-400">{message.username}</p>
+        <p
+          className={`text-[0.65rem px-[2px] pb-[2px] ${
+            isUser ? "text-blue-400 text-right" : "text-red-400 text-left"
+          }`}
+        >
+          {message.username}
+        </p>
 
         <div className="flex items-end">
-          <div className="px-3 py-2 rounded-lg w-fit text-white bg-red-400">
+          <div
+            className={`px-3 py-2 rounded-lg w-fit text-white ${
+              isUser ? "bg-blue-400 ml-auto order-2" : "bg-red-400"
+            }`}
+          >
             <p>{message.message}</p>
           </div>
-          <p>{new Date(message.created_at).toLocaleDateString()}</p>
+          <p
+            className={`text-[0.65rem] italic px-2 text-gray-300 ${
+              isUser && "text-right`
+            }`}
+          >
+            {new Date(message.created_at).toLocaleString()}
+          </p>
         </div>
       </div>
     </div>
